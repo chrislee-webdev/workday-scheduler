f612589e9659b51c8592068f995d62ee36a8875b
Christopher <leechrs1@gmail.com>
2022-04-03 19:11:03 -0500

    working on date display

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//current day
+let momentDate = moment().format();
+let currentDay = document.getElementById("currentDay");
+
+currentDay.innerHTML = momentDate;
+
 $(".activity").sortable({
-    
+
 })
