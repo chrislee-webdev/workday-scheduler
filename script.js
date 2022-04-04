d3d5e08511f8a3078f0d1ba61b9e901ae996d0e3
Christopher <leechrs1@gmail.com>
2022-04-04 18:43:17 -0500

    changed class names to utiliz preset styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//current day
 // moment().format("MMM Do YYYY");
 let momentDate = moment().format('MMM Do yyyy');
 let currentDay = document.getElementById("currentDay");
-
+// current date display on page
 currentDay.innerHTML = momentDate;
+//current time
+var currentTime = moment()
+
