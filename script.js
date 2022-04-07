081880adac487cf39983e8d929acafa9e054b5ec
Christopher <leechrs1@gmail.com>
2022-04-06 21:42:01 -0500

    got each button to work, save the values, and display on reload

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ var currentHour = moment().hours();
 var scheduleTime = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 // variable to hold row as a variable
 var row = document.getElementsByClassName("row");
-//variable to hold button element
+//variables to hold button element
 var saveBtn = document.getElementById("saveBtn");
-//variable to hold time-block
-var timeBlock = document.getElementsByClassName("time-block");
+var saveBtn10 = document.getElementById("saveBtn-10")
+var saveBtn10 = document.getElementById("saveBtn-11")
+var saveBtn10 = document.getElementById("saveBtn-12")
+var saveBtn10 = document.getElementById("saveBtn-13")
+var saveBtn10 = document.getElementById("saveBtn-14")
+var saveBtn10 = document.getElementById("saveBtn-15")
+var saveBtn10 = document.getElementById("saveBtn-16")
+var saveBtn10 = document.getElementById("saveBtn-17")
 
 console.log(currentHour);
 
@@ -89,7 +95,69 @@ if (currentHour > 17) {
     $('#17').addClass("present")
 }
 
-saveBtn.addEventListener("click", function(){
+//Hour 9 function
+$('#saveBtn').click(function(){
     var textValue = document.getElementById("description").value;
-    localStorage.setItem("key", textValue);
+    //set local storge
+    localStorage.setItem("key", textValue)
 })
+//Hour 10 function
+$('#saveBtn-10').click(function(){
+    var textValue = document.getElementById("description-10").value;
+    //set local storge
+    localStorage.setItem("key10", textValue)
+})
+//Hour 11 function
+$('#saveBtn-11').click(function(){
+    var textValue = document.getElementById("description-11").value;
+    //set local storge
+    localStorage.setItem("key11", textValue)
+})
+//Hour 12 function
+$('#saveBtn-12').click(function(){
+    var textValue = document.getElementById("description-12").value;
+    //set local storge
+    localStorage.setItem("key12", textValue)
+})
+//Hour 13 function
+$('#saveBtn-13').click(function(){
+    var textValue = document.getElementById("description-13").value;
+    //set local storge
+    localStorage.setItem("key13", textValue)
+})
+//Hour 14 function
+$('#saveBtn-14').click(function(){
+    var textValue = document.getElementById("description-14").value;
+    //set local storge
+    localStorage.setItem("key14", textValue)
+})
+//Hour 15 function
+$('#saveBtn-15').click(function(){
+    var textValue = document.getElementById("description-15").value;
+    //set local storge
+    localStorage.setItem("key15", textValue)
+})
+//Hour 16 function
+$('#saveBtn-16').click(function(){
+    var textValue = document.getElementById("description-16").value;
+    //set local storge
+    localStorage.setItem("key16", textValue)
+})
+//Hour 17 function
+$('#saveBtn-17').click(function(){
+    var textValue = document.getElementById("description-17").value;
+    //set local storge
+    localStorage.setItem("key17", textValue)
+})
+
+
+    //get local storage
+    document.getElementById("description").innerHTML = localStorage.getItem("key");
+    document.getElementById("description-10").innerHTML = localStorage.getItem("key10");
+    document.getElementById("description-11").innerHTML = localStorage.getItem("key11");
+    document.getElementById("description-12").innerHTML = localStorage.getItem("key12");
+    document.getElementById("description-13").innerHTML = localStorage.getItem("key13");
+    document.getElementById("description-14").innerHTML = localStorage.getItem("key14");
+    document.getElementById("description-15").innerHTML = localStorage.getItem("key15");
+    document.getElementById("description-16").innerHTML = localStorage.getItem("key16");
+    document.getElementById("description-17").innerHTML = localStorage.getItem("key17");
